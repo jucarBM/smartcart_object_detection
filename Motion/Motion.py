98946c10b82f182ccb00bdceb92a2d8859f9dd10
Sebasvb <sebastian.venero@utec.edu.pe>
2022-05-22 11:50:09 -0500

    añadiomotion_con morfo y blur, agregue contornos

--- Motion/Motion.py
+++ Motion/Motion.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Video Capture 
-capture = cv2.VideoCapture(1)
+capture = cv2.VideoCapture(0)
 
 
 # History, Threshold, DetectShadows 
@@ -27,25 +27,45 @@ while(1):
 	# Get the foreground mask
 	fgmask = fgbg.apply(resizedFrame)
     # ------------------------------------------------
-	gauss_filter = cv2.GaussianBlur(fgmask,(5,5),3)
-	median_blur = cv2.medianBlur(fgmask,5)
+	# gauss_filter = cv2.GaussianBlur(fgmask,(5,5),3)
+	# median_blur = cv2.medianBlur(fgmask,5)
 
-	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
-	cierre = cv2.morphologyEx(gauss_filter,cv2.MORPH_CLOSE,kernel) 
-    # ----------------------------------------------------------------
+	# kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
+    # cierre = cv2.morphologyEx(gauss_filter,cv2.MORPH_CLOSE,kernel) 
+	# dilate = cv2.dilate(gauss_filter,None,iterations=2) 
+    # # ----------------------------------------------------------------
 	# Count all the non zero pixels within the mask
-	count = np.count_nonzero(cierre)
+	count = np.count_nonzero(fgmask)
 
 	print('Frame: %d, Pixel Count: %d' % (frameCount, count))
 
 	# Determine how many pixels do you want to detect to be considered "movement"
 	
 	if (frameCount > 1 and count > 5000 and count < 9000):
+        # find contours or continuous white blobs in the image
+		contours, hierarchy = cv2.findContours(fgmask.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+		# find the index of the largest contour
+		if len(contours) > 0:
+			areas = [cv2.contourArea(c) for c in contours]
+			max_index = np.argmax(areas)
+			cnt=contours[max_index]   
+
+			# draw a bounding box/rectangle around the largest contour
+			x,y,w,h = cv2.boundingRect(cnt)
+			cv2.rectangle(resizedFrame,(x,y),(x+w,y+h),(0,255,0),2)
+			area = cv2.contourArea(cnt)
+
+			# print area to the terminal
+			print(area)
+		
+			# add text to the frame
+			cv2.putText(resizedFrame, "Largest Contour", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 		print('detect')
 		cv2.putText(resizedFrame, 'detect', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
 	cv2.imshow('Frame', resizedFrame)
-	cv2.imshow('Mask', cierre)
+	cv2.imshow('Mask', fgmask)
 
 
 	k = cv2.waitKey(1) & 0xff

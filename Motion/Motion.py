9fde13d7f0bb2375001bf3f1d7dd4d095bf43f9e
Sebasvb <sebastian.venero@utec.edu.pe>
2022-05-22 09:51:41 -0500

    añadiomotion_con morfo y blur

--- Motion/Motion.py
+++ Motion/Motion.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 # Video Capture 
-capture = cv2.VideoCapture(0)
+capture = cv2.VideoCapture(1)
 
 
 # History, Threshold, DetectShadows 
@@ -26,20 +26,26 @@ while(1):
 
 	# Get the foreground mask
 	fgmask = fgbg.apply(resizedFrame)
+    # ------------------------------------------------
+	gauss_filter = cv2.GaussianBlur(fgmask,(5,5),3)
+	median_blur = cv2.medianBlur(fgmask,5)
 
+	kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(15,15))
+	cierre = cv2.morphologyEx(gauss_filter,cv2.MORPH_CLOSE,kernel) 
+    # ----------------------------------------------------------------
 	# Count all the non zero pixels within the mask
-	count = np.count_nonzero(fgmask)
+	count = np.count_nonzero(cierre)
 
 	print('Frame: %d, Pixel Count: %d' % (frameCount, count))
 
 	# Determine how many pixels do you want to detect to be considered "movement"
-	# if (frameCount > 1 and cou`nt > 5000):
-	if (frameCount > 1 and count > 5000):
+	
+	if (frameCount > 1 and count > 5000 and count < 9000):
 		print('detect')
 		cv2.putText(resizedFrame, 'detect', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
 	cv2.imshow('Frame', resizedFrame)
-	cv2.imshow('Mask', fgmask)
+	cv2.imshow('Mask', cierre)
 
 
 	k = cv2.waitKey(1) & 0xff

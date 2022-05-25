4d0a31d29c0212f7287c33bb216ad66bfe813271
Juan Carlos <jucar251096@gmail.com>
2022-05-24 20:03:55 -0500

    comunication beetween two cameras implemented

--- Tools/n_cameras/main.py
+++ Tools/n_cameras/main.py
@@ -1,11 +1,14 @@
 # main of the n_cameras tool
-from mul_camera import Camera
+from mul_camera import Camera, compare2cameras
+from threading import Thread
 # init function of python script
 
 if __name__ == "__main__":
     # create camera objects
-    cam1 = Camera(0, "zero", "other")
-    cam2 = Camera(1, "one", "other")
+    cam1 = Camera(0, "zero", "normal")
+    cam2 = Camera(1, "one", "normal")
+    main_thread = Thread(target=compare2cameras, args=(cam1, cam2,))
     # run the cameras
     cam1.start()
     cam2.start()
+    main_thread.start()
